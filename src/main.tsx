9a030de90cf2c658851fd60d92c0f4ac9d2a28f4
Eric Clemmons <eric@smarterspam.com>
2025-09-23 12:44:58 -0500

    Working tray icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
+import { TrayIcon } from '@tauri-apps/api/tray';
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
+import { defaultWindowIcon } from '@tauri-apps/api/app';
+import { Menu } from '@tauri-apps/api/menu';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+const menu = await Menu.new({
+  items: [
+    { id: 'quit', text: 'Quit',}
+  ]
+})
+
+await TrayIcon.new({
+  icon: await defaultWindowIcon() ?? '',
+  menu,
+  menuOnLeftClick: true,
+});
